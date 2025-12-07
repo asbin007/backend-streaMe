e1d145ecb0319bc3496e961e9664a424e881fac1
asbin007 <asbingamer@gmail.com>
2025-12-08 01:06:16 +0545

    fix otp by resend

--- src/services/sendMail.ts
+++ src/services/sendMail.ts
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 import config from '../config/config';
 
 interface MailOptions {
@@ -8,30 +8,22 @@ interface MailOptions {
   html?: string;
 }
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 const sendMail = async (options: MailOptions): Promise<boolean> => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true, // true for 465, false for other ports
-      auth: {
-        user: config.email,
-        pass: config.password,
-      },
-    });
-
-    const info = await transporter.sendMail({
-      from: `"stream me " <${config.email}>`,
+    const response = await resend.emails.send({
+      from: "Stream Me <noreply@yourdomain.com>", // IMPORTANT
       to: options.to,
       subject: options.subject,
       text: options.text,
       html: options.html,
     });
 
-    console.log('Email sent successfully:', info.messageId);
+    console.log("Email sent:", response);
     return true;
   } catch (error) {
-    console.error('Error sending email with Nodemailer:', error);
+    console.error("Resend email error:", error);
     return false;
   }
 };
